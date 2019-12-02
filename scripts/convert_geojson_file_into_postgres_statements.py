e1c1df7ac786bfc12a7713e75bbf6fed110b95e8
Liang Cheng <liangcheng6@gmail.com>
2019-12-02 13:19:34 -0800

    update

---
+++ scripts/convert_geojson_file_into_postgres_statements.py
@@ -0,0 +1,52 @@
+import json
+
+def process_single_feature(feature):
+    processed_feature = {}
+    processed_feature["EGID"] = feature["properties"]["EGID"]
+    processed_feature["ROOF_TYPE"] = feature["properties"]["ROOF_TYPE"]
+    processed_feature["Shape_Length"] = feature["properties"]["Shape_Length"]
+    processed_feature["Shape_Area"] = feature["properties"]["Shape_Area"]
+    processed_feature["ALTITUDE_M"] = feature["properties"]["ALTITUDE_M"]
+    processed_feature["geometry"] = feature["geometry"]
+    processed_feature["geometry"]["coordinates"] = processed_feature["geometry"]["coordinates"][0][0]
+    return processed_feature
+
+
+def form_insert_statement(processed_feature):
+    EGID = processed_feature["EGID"]
+
+    Shape_Area = processed_feature["Shape_Area"]
+    Altitude_M = processed_feature["ALTITUDE_M"]
+    RoofType = processed_feature["ROOF_TYPE"]
+    Shape_Length = processed_feature["Shape_Length"]
+    sql_statement = f"""
+insert into BuildingInfo (EGID, Shape_Area, Altitude_M, RoofType, Shape_Length, Border)
+VALUES (
+    '{EGID}',
+    '{Shape_Area}',
+    '{Altitude_M}',
+    '{RoofType}',
+    '{Shape_Length}',
+    ST_GeomFromGeoJSON(
+        '{{
+            "type": "Multipolygon",
+            "coordinates": {processed_feature["geometry"]["coordinates"]}
+        }}'
+    )
+);
+    """
+    return sql_statement
+
+
+with open('../data/DC_buildings_Footprint_4326_training.geojson') as json_file:
+    data = json.load(json_file)
+    features = data["features"]
+    # print ("count of features: ", len(features))
+
+
+    for feature in features:
+        processed_feature_example = process_single_feature(feature)
+        # print (processed_feature_example)
+
+        sql_example = form_insert_statement(processed_feature_example)
+        print (sql_example)
