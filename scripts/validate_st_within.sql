f903061a067317cba091aa70244306959fe67788
Liang Cheng <liangcheng6@gmail.com>
2019-12-02 17:58:44 -0800

    update

--- scripts/validate_st_within.sql
+++ scripts/validate_st_within.sql
@@ -1,3 +1,9 @@
 CREATE TABLE building_centroid AS
 SELECT b.egid, st_centroid(b.border) as centroid
 FROM buildinginfotraining b
+
+
+--the 2 egid entries should be the same
+select c.egid, b.egid
+from buildinginfotraining b, building_centroid c
+where st_within(c.centroid, b.border)
