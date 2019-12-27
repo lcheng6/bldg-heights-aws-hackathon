99204b11e0092b9609ce153f0788f960849a5dfb
Liang Cheng <liangcheng6@gmail.com>
2019-12-27 10:21:36 -0500

    reference file for using docker entrypoint and first version of independent lidar processing script

--- lidar-processing/batch-processing/las-to-sql-processor.py
+++ lidar-processing/batch-processing/las-to-sql-processor.py
@@ -5,17 +5,18 @@ from geopandas import GeoDataFrame
 from shapely.geometry import Point
 import requests
 import os
+import lasutility
 
 import boto3 # like, are we not going to use boto3 in python on AWS?
 from botocore.exceptions import ClientError
 
-for laskeyname in lasKeynames:
-    download_las_data(laskeyname)
-    row_count = convert_las_data_to_sql_statement(laskeyname)
+def las_to_sql(laskeyname):
+    lasutility.download_las_data(laskeyname)
+    row_count = lasutility.convert_las_data_to_sql_statement(laskeyname)
     print(row_count)
-    cleanup_las_data(laskeyname)
+    lasutility.cleanup_las_data(laskeyname)
 
     keysequencenum = laskeyname.split('.')[0] #get the #### of the file "####.las"
     sqlfilename = f"{keysequencenum}.sql"
-    upload_las_data_to_s3_bucket(sqlfilename, "additional-test-datasets", "bldg-height/las-sql-statements")
+    lasutility.upload_las_data_to_s3_bucket(sqlfilename, "additional-test-datasets", "bldg-height/las-sql-statements")
 
