496959b243be79b71bc314dd43c279b6e4948470
Liang Cheng <liangcheng6@gmail.com>
2020-01-22 22:45:54 -0500

    update index creation to increase point matching performance

--- scripts/ddl-statements/create_gis_indices.sql
+++ scripts/ddl-statements/create_gis_indices.sql
@@ -1,8 +1,14 @@
 -- run these indices creation for lookup performance improvement
 
-CREATE INDEX idx_building_info_training_border ON buildinginfotraining USING gist (border);
-CREATE INDEX idx_building_test_training_border ON buildinginfotest USING gist (border);
-CREATE INDEX idx_lidar_value_grnd_coord ON lidar_values USING gist (ground_coord);
+CREATE INDEX if not exists idx_building_info_training_border ON buildinginfotraining USING gist (border);
+CREATE INDEX if not exists idx_building_test_training_border ON buildinginfotest USING gist (border);
+CREATE INDEX if not exists idx_lidar_value_grnd_coord ON lidar_values USING gist (ground_coord);
+
+
+-- run these indices to help with building information lookup
+
+CREATE INDEX if not exists idx_lidarbuildingmatchtest_bldgid ON lidarbuildingmatchtest (bldgid);
+-- CREATE INDEX if not exists idx_lidarbuildingmatchtraining_bldgid ON lidarbuildingmatchtraining (bldgid);
 
 -- activate all index and improve search performance
 REINDEX DATABASE lidarbuilding
