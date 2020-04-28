b4cfce8e13c8bf2cec5d20f6f6db481d4fc83d1a
Liang Cheng <liangcheng6@gmail.com>
2020-04-28 08:42:35 -0400

    more sql

---
+++ scripts/ddl-statements/create-building-detailed-info-within-ajjits-selected-tracts.sql
@@ -0,0 +1,8 @@
+create table building_detailed_info_within_ajjits_selected_tracts as
+    select bldg.id as bldgid, bldg.egid as egid, bldg.shape_area as shape_area,
+           bldg.altitude_m as altitude_m,
+           bldg.rooftype as rooftype, bldg.shape_length as shape_length,
+           bldg.border as border,
+           dc.geoid, dc.name, dc.namelsad
+    from buildinginfotest bldg, dctracts dc
+    where st_within(st_centroid(bldg.border), dc.border) and dc.geoid in ('11001005600', '11001010800',  '11001010700', '11001005500');
