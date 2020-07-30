84101726097fec4ac2c97fee1d3c983aa4ef9548
Liang Cheng <liangcheng6@gmail.com>
2020-07-30 10:54:55 -0400

    update

--- scripts/postgres-ddl-statements/050_match_lidar_points_to_bldg_table.sql
+++ scripts/postgres-ddl-statements/050_match_lidar_points_to_bldg_table.sql
@@ -1,3 +1,5 @@
+-- Update all statistics on all existing tables
+vacuum analyze
 -- Select the data points from buildinginfotest
 create table lidarbuildingmatchtest as
     select bldg.egid as bldg_egid, las.id as las_id
