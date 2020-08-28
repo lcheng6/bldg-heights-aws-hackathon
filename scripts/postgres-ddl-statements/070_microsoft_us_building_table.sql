27de313a1ba1a0e65a962291d737b10bf3eb4acb
Liang Cheng <liangcheng6@gmail.com>
2020-08-28 15:56:40 -0400

    change the size of state name width

--- scripts/postgres-ddl-statements/070_microsoft_us_building_table.sql
+++ scripts/postgres-ddl-statements/070_microsoft_us_building_table.sql
@@ -2,7 +2,7 @@ drop table if exists "lidarbuilding".public.ms_building_borders;
 
 create table "lidarbuilding".public.ms_building_borders (
     id serial primary key,
-    State VARCHAR (12) not null,
+    State VARCHAR (64) not null,
     Border geometry,
     BorderCentroid geometry
 );
